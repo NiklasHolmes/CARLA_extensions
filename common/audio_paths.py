c5eb42139694b82070fd2bd0887b13f0e27e6239
NiklasHolmes <niklas.fraissl@gmail.com>
2026-05-25 22:31:51 +0200

    add audio path file for songs

---
+++ common/audio_paths.py
@@ -0,0 +1,35 @@
+"""Central audio path registry for CARLA extensions."""
+
+# Anger
+ANGER_C_O_FORTUNA_PATH = r".\audio\songs\Anger_C_Carl Orff - O Fortuna ~ Carmina Burana 4.mp3"
+ANGER_H_X_GON_GIVE_IT_TO_YA_PATH = r".\audio\songs\Anger_H_DMX - X Gon' Give It To Ya 4.mp3"
+ANGER_RP_MASTER_OF_PUPPETS_PATH = r".\audio\songs\Anger_RP_Master of Puppets (Remastered) 4.mp3"
+
+# Fear
+FEAR_C_STRING_QUARTET_OP_3_PATH = r".\audio\songs\Fear_C_Alban Berg - String Quartet, Op. 3.mp3"
+FEAR_H_WARNING_PATH = r".\audio\songs\Fear_H_Warning.mp3"
+FEAR_RP_NEUROSIS_FEAR_AND_SICKNESS_PATH = r".\audio\songs\Fear_RP_Neurosis - Fear & Sickness.mp3"
+
+# Happiness
+HAPPINESS_C_EINE_KLEINE_NACHTMUSIK_PATH = r".\audio\songs\Happiness_C_Eine Kleine Nachtmusik - Mozart 4.mp3"
+HAPPINESS_H_GANGSTAS_PARADISE_PATH = r".\audio\songs\Happiness_H_Coolio - Gangsta's Paradise (feat. L.V.) [Official Music Video] 4.mp3"
+HAPPINESS_RP_UPTOWN_FUNK_PATH = r".\audio\songs\Happiness_RP_Mark Ronson - Uptown Funk (Official Video) ft. Bruno Mars 4.mp3"
+
+# Neutral
+NEUTRAL_C_ARABESQUE_NO_1_PATH = r".\audio\songs\Neutral_C_Debussy - Arabesque No. 1 4.mp3"
+NEUTRAL_C_SHOW_IMPROMPTU_OP_90_NO_2_PATH = r".\audio\songs\Neutral_C_SHOW_Schubert - Impromptu Op. 90 No. 2 in E♭ Major.mp3"
+NEUTRAL_H_APER_PATH = r".\audio\songs\Neutral_H_Aper (feat. Beatmund Noise).mp3"
+NEUTRAL_H_SHOW_AURORA_PATH = r".\audio\songs\Neutral_H_SHOWAurora.mp3"
+NEUTRAL_RP_SHOW_LET_IT_BE_PATH = r".\audio\songs\Neutral_RP_SHOW_The Beatles - Let It Be 4.mp3"
+NEUTRAL_RP_TRACY_CHAPMAN_FAST_CAR_PATH = r".\audio\songs\Neutral_RP_Tracy Chapman - Fast Car (Official Music Video) 4.mp3"
+
+# Sadness
+SADNESS_C_MONDSCHEINSONATE_PATH = r".\audio\songs\Sadness_C_Beethoven - Mondscheinsonate.mp3"
+SADNESS_H_MOCKINGBIRD_PATH = r".\audio\songs\Sadness_H_Eminem - Mockingbird [Official Music Video] 4.mp3"
+SADNESS_RP_MAD_WORLD_PATH = r".\audio\songs\Sadness_RP__Mad World_ (feat. Gary Jules) - Official Music Video 4.mp3"
+
+# Surprise
+SURPRISE_C_THE_MARRIAGE_OF_FIGARO_OVERTURE_PATH = r".\audio\songs\Surprise_C_Mozart - The Marriage of Figaro (Overture) 4.mp3"
+SURPRISE_H_WHERE_IS_THE_LOVE_PATH = r".\audio\songs\Surprise_H_The Black Eyed Peas - Where Is The Love_ (Official Music Video) 4.mp3"
+SURPRISE_RP_LITTLE_NUMBERS_PATH = r".\audio\songs\Surprise_RP_Wonder_BOY - Little Numbers (Official Video).mp3"
+
